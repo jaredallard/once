71b6d44ad81733176ff7f10e342113d1f451d281
Jared Allard <jaredallard@users.noreply.github.com>
2026-03-05 15:38:11 -0800

    fix(deps): stencil-golang post-forgejo migration

--- cmd/once/once.go
+++ cmd/once/once.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 once contributors
+// Copyright (C) 2026 once contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
